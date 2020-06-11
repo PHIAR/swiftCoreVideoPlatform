2ddce217393090f2f97cdddee1e33fd8f73420bc
Aaditya Chandrasekhar <aaditya.c@phiar.net>
2020-06-11 14:22:40 -0700

    Use dynamic library target type for AVF, CV, CM and VT.

--- Package.swift
+++ Package.swift
@@ -5,18 +5,22 @@ import PackageDescription
 let package = Package(name: "CoreVideo",
                       products: [
                           .library(name: "AVFoundation",
+                                   type: .dynamic,
                                    targets: [
                                        "AVFoundation",
                                    ]),
                           .library(name: "CoreMedia",
+                                   type: .dynamic,
                                    targets: [
                                        "CoreMedia",
                                    ]),
                           .library(name: "CoreVideo",
+                                   type: .dynamic,
                                    targets: [
                                        "CoreVideo",
                                    ]),
                           .library(name: "VideoToolbox",
+                                   type: .dynamic,
                                    targets: [
                                        "VideoToolbox",
                                    ]),
