a082e8ce618369c4980f929c9afa66f777555561
Aaditya Chandrasekhar <aaditya.c@phiar.net>
2020-06-21 14:06:37 -0700

    Add interface for AVCaptureVideoDataOutputSampleBufferDelegate.

--- Sources/AVFoundation/AVCaptureVideoDataOutputSampleBufferDelegate.swift
+++ Sources/AVFoundation/AVCaptureVideoDataOutputSampleBufferDelegate.swift
@@ -1,4 +1,25 @@
+import CoreMedia
 import Foundation
 
 public protocol AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput,
+                       didDrop sampleBuffer: CMSampleBuffer,
+                       from connection: AVCaptureConnection)
+
+    func captureOutput(_ output: AVCaptureOutput,
+                       didOutput sampleBuffer: CMSampleBuffer,
+                       from connection: AVCaptureConnection)
+}
+
+public extension AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput,
+                       didDrop sampleBuffer: CMSampleBuffer,
+                       from connection: AVCaptureConnection) {
+    }
+
+    func captureOutput(_ output: AVCaptureOutput,
+                       didOutput sampleBuffer: CMSampleBuffer,
+                       from connection: AVCaptureConnection) {
+    }
+
 }
